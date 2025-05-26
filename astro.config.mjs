d92f0c1fe29b80dfb9a8905b121fc9aa162378ba
Juan Antonio <romerixo@gmail.com>
2025-05-26 09:36:13 +0200

    Add @astrojs/vercel dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import vercelStatic from '@astrojs/vercel/static';
+
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  output: 'static',
+  adapter: vercelStatic(),
 });
